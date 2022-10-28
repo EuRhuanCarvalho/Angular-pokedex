71642eedc5dc0ccbd9fdb459427408950476c3bd
EuRhuanCarvalho <rhuan_eugenio@hotmail.com>
2022-10-27 23:57:18 -0300

    Estruturando pokedex

--- angular-pokedex/src/app/pokemon-card/pokemon-card.component.ts
+++ angular-pokedex/src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
 
-  constructor() { }
+  @Input()
+  pokemon!: string;
 
-  ngOnInit(): void {
+  @Input()
+  numero!: number;
+
+  pegarImagemPokemon() {
+    const numeroFormatado = this.leadingZero(this.numero);
+
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numeroFormatado}.png`;
   }
 
+  leadingZero(str: string | number, size = 3 ): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+
+    return s
+
+
+  }
 }
